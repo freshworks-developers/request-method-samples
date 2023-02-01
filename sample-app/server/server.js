4547c66cf80888bf6c60a1b9cbb7cd0196304827
zachjonesnoel <joneszachariah.noeln@freshworks.com>
2023-02-01 08:53:32 +0530

    Updated iparams request template

--- sample-app/server/server.js
+++ sample-app/server/server.js
@@ -4,13 +4,17 @@ exports = {
     ],
     onTicketCreateCallback: async function (payload) {
         let ticket = payload.data.ticket
-        await $request.invokeTemplate("replyTicket", {
-            context: {
-                id: ticket.id
-            },
-            body: JSON.stringify({ 
-                "body": "Thanks for reporting, while this is an automated reply we are working on resolving it." 
+        try {
+            await $request.invokeTemplate("replyTicket", {
+                context: {
+                    id: ticket.id
+                },
+                body: JSON.stringify({
+                    "body": "Thanks for reporting, while this is an automated reply we are working on resolving it."
+                })
             })
-        })
+        } catch (error) {
+            console.error(error)
+        }
     }
 }

e26bed8585c5ce44064933f4864f99b30df0b6b2
zachjonesnoel <joneszachariah.noeln@freshworks.com>
2023-02-15 18:03:19 +0530

    Code with prettier

--- request-template-with-oauth/README.md
+++ request-template-with-oauth/README.md
@@ -1,2 +1,3 @@
 ### Request templates sample app
+
 Sample app to demonstrate the usage of request templates in both front-end (`client.request.invokeTemplate()`) with OAuth authentication with Asana.
--- request-template-with-oauth/app/scripts/app.js
+++ request-template-with-oauth/app/scripts/app.js
@@ -2,15 +2,18 @@ var client;
 
 (async function init() {
   client = await app.initialized();
-  client.events.on('app.activated', renderAsanaInfo);
+  client.events.on("app.activated", renderAsanaInfo);
 })();
 
 //To demonstrate a simple GET request
 async function getWorkspace() {
-  const workspaceElement = document.querySelector('#workspacesCount');
+  const workspaceElement = document.querySelector("#workspacesCount");
   try {
-    let workspace = await client.request.invokeTemplate("asanaGetWorkspace", {})
-    let workspaceJSON = JSON.parse(workspace.response)
+    let workspace = await client.request.invokeTemplate(
+      "asanaGetWorkspace",
+      {}
+    );
+    let workspaceJSON = JSON.parse(workspace.response);
     workspaceElement.innerHTML = workspaceJSON.data.length;
   } catch (err) {
     workspaceElement.innerHTML = `-` + err;
@@ -18,5 +21,5 @@ async function getWorkspace() {
 }
 
 async function renderAsanaInfo() {
-  await getWorkspace()
+  await getWorkspace();
 }
